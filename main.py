8edd3dd951dfac927c54cce4ddc0e9a732e24947
Abdullah <abdullahsheikh0945@gmail.com>
2024-05-28 11:45:26 +0530

    dvc_added

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from cnnClassifier import logger 
 from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline 
-from cnnClassifier.pipeline.stage_02_proper_base_model import PrepareBaseModelTrainingPipeline
+from cnnClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 from cnnClassifier.pipeline.stage_03_model_trainer import ModelTrainingPipeline
 from cnnClassifier.pipeline.stage_04_model_evaluation import EvaluationPipeline
 
